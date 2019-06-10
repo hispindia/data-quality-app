2e9e8217710b94cdb158838d44a9438c57cd5d65
SamtaPandey <samta.pandey@hispindia.org>
2019-06-10 15:53:04 +0530

    outlier app new tab link

--- src/pages/outlierAnalysisApp/OutlierAnalysisApp.js
+++ src/pages/outlierAnalysisApp/OutlierAnalysisApp.js
@@ -3,7 +3,7 @@ import { PureComponent } from 'react';
 class OutlierAnalysisApp extends PureComponent {
     render() {
         return (
-            window.open('https://devtest.hispindia.org/upupgrade/api/apps/Outlier-Analysis/index.html')
+            window.open('../api/apps/Outlier-Analysis/index.html', '_blank'), window.location.assign('../dhis-web-data-quality/index.action#/'), window.location.reload()
         );
     }
 }
