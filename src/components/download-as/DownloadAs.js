eade014d2dd8beeccd23d3aaf6a2a359f7debc31
Sourabh Bhardwaj <bsourabh191@gmail.com>
2020-09-24 12:07:11 +0530

    download issue fix related to previous history

--- src/components/download-as/DownloadAs.js
+++ src/components/download-as/DownloadAs.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 /* i18n */
 import i18n from '../../locales';
 import { i18nKeys } from '../../i18n';
-
+import ClearCache from "react-clear-cache";
+import { useClearCache } from "react-clear-cache";
 import styles from './DownloadAs.css';
 
 class DownloadAs extends PureComponent {
@@ -18,27 +19,28 @@ class DownloadAs extends PureComponent {
 
     render() {
         const api = this.context.d2.Api.getApi();
-        const timestamp = new Date().getTime();       // To clear cache
+        let timestamp =Date.now()// To clear cache
         return (
             <div className={styles.downloadAs}>
-                <a
-                    className="export-pdf-action"
-                    href={`${api.baseUrl}${this.props.endpoint}.pdf?t=${timestamp}`}
-                    target="_blank"
-                >
-                    {i18n.t(i18nKeys.downloadAs.pdf)}
-                </a>
-                <a
-                    className="export-xls-action"
-                    href={`${api.baseUrl}${this.props.endpoint}.xls?t=${timestamp}`}
-                    target="_blank"
-                >
-                    {i18n.t(i18nKeys.downloadAs.xls)}
-                </a>
+                {/*<a*/}
+                {/*    className="export-pdf-action"*/}
+                {/*    href={`${api.baseUrl}${this.props.endpoint}.pdf?t=${timestamp}`}*/}
+                {/*    target="_blank"*/}
+                {/*>*/}
+                {/*    {i18n.t(i18nKeys.downloadAs.pdf)}*/}
+                {/*</a>*/}
+                {/*<a*/}
+                {/*    className="export-xls-action"*/}
+                {/*    href={`${api.baseUrl}${this.props.endpoint}.xls?t=${timestamp}`}*/}
+                {/*    target="_blank"*/}
+                {/*>*/}
+                {/*    {i18n.t(i18nKeys.downloadAs.xls)}*/}
+                {/*</a>*/}
+
                 <a
                     className="export-csv-action"
-                    href={`${api.baseUrl}${this.props.endpoint}.csv?t=${timestamp}`}
-                    target="_blank"
+                    href={  `${api.baseUrl}${this.props.endpoint}`}
+                    target="_top"
                 >
                     {i18n.t(i18nKeys.downloadAs.csv)}
                 </a>
